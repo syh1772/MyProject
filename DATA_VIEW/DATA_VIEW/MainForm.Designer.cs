8d4b7d9187469c2f05097949677290f9c3ef69b4
Hoya <syh1772@naver.com>
2025-12-12 08:45:48 +0900

    최초 Push

---
+++ DATA_VIEW/DATA_VIEW/MainForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace SEMCNS.DATA_VIEW
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.pnlStf = new System.Windows.Forms.Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPos = new System.Windows.Forms.Label();
+            this.ckbGroupingVia = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtToleranceX = new System.Windows.Forms.TextBox();
+            this.txtToleranceY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(32, 26);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(96, 40);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "폴더 선택";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPath.Location = new System.Drawing.Point(134, 26);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(719, 61);
+            this.lblPath.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClose.Location = new System.Drawing.Point(1013, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(129, 40);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "프로그램 종료";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // picDisplay
+            // 
+            this.picDisplay.Location = new System.Drawing.Point(413, 90);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(376, 332);
+            this.picDisplay.TabIndex = 4;
+            this.picDisplay.TabStop = false;
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(32, 90);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowTemplate.Height = 23;
+            this.dgvMain.Size = new System.Drawing.Size(375, 580);
+            this.dgvMain.TabIndex = 2;
+            this.dgvMain.SelectionChanged += new System.EventHandler(this.dgvMain_SelectionChanged);
+            // 
+            // pnlStf
+            // 
+            this.pnlStf.Location = new System.Drawing.Point(795, 90);
+            this.pnlStf.Name = "pnlStf";
+            this.pnlStf.Size = new System.Drawing.Size(347, 332);
+            this.pnlStf.TabIndex = 6;
+            this.pnlStf.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlStf_Paint);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
+            this.lblName.Location = new System.Drawing.Point(419, 432);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(84, 25);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Name : ";
+            // 
+            // lblPos
+            // 
+            this.lblPos.AutoSize = true;
+            this.lblPos.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
+            this.lblPos.Location = new System.Drawing.Point(419, 457);
+            this.lblPos.Name = "lblPos";
+            this.lblPos.Size = new System.Drawing.Size(64, 25);
+            this.lblPos.TabIndex = 9;
+            this.lblPos.Text = "Pos : ";
+            // 
+            // ckbGroupingVia
+            // 
+            this.ckbGroupingVia.AutoSize = true;
+            this.ckbGroupingVia.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.ckbGroupingVia.Location = new System.Drawing.Point(14, 27);
+            this.ckbGroupingVia.Name = "ckbGroupingVia";
+            this.ckbGroupingVia.Size = new System.Drawing.Size(229, 25);
+            this.ckbGroupingVia.TabIndex = 10;
+            this.ckbGroupingVia.Text = "인접한 비아 제외 기능 사용";
+            this.ckbGroupingVia.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(17, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "X 범위 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(17, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Y 범위 : ";
+            // 
+            // txtToleranceX
+            // 
+            this.txtToleranceX.Location = new System.Drawing.Point(94, 64);
+            this.txtToleranceX.Name = "txtToleranceX";
+            this.txtToleranceX.Size = new System.Drawing.Size(53, 23);
+            this.txtToleranceX.TabIndex = 13;
+            this.txtToleranceX.Text = "1.5";
+            this.txtToleranceX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtToleranceX_KeyPress);
+            // 
+            // txtToleranceY
+            // 
+            this.txtToleranceY.Location = new System.Drawing.Point(94, 94);
+            this.txtToleranceY.Name = "txtToleranceY";
+            this.txtToleranceY.Size = new System.Drawing.Size(53, 23);
+            this.txtToleranceY.TabIndex = 14;
+            this.txtToleranceY.Text = "1.5";
+            this.txtToleranceY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtToleranceX_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(153, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 21);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "mm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(153, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 21);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "mm";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckbGroupingVia);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtToleranceY);
+            this.groupBox1.Controls.Add(this.txtToleranceX);
+            this.groupBox1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(795, 432);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 218);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "기능";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnExcel.Location = new System.Drawing.Point(859, 26);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(129, 40);
+            this.btnExcel.TabIndex = 18;
+            this.btnExcel.Text = "엑셀로 변환";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 682);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblPos);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pnlStf);
+            this.Controls.Add(this.picDisplay);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Panel pnlStf;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPos;
+        private System.Windows.Forms.CheckBox ckbGroupingVia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtToleranceX;
+        private System.Windows.Forms.TextBox txtToleranceY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExcel;
+    }
+}
+
---
+++ DATA_VIEW/DATA_VIEW/MainForm.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SEMCNS.DATA_VIEW
+{
+    public partial class MainForm : Form
+    {
+        public class Via
+        {
+            public string Name { get; set; }
+            public double X { get; set; }
+            public double Y { get; set; }
+            public string ImagePath { get; set; }
+
+            public Via(string name, double x, double y, string imagePath)
+            {
+                this.Name = name;
+                this.X = x;
+                this.Y = y;
+                this.ImagePath = imagePath;
+            }
+        }
+
+        Dictionary<string, Via> _viaDic;
+        string _currentFileName = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _viaDic = new Dictionary<string, Via>();
+
+            dgvMain.Columns.Add("Name", "Name");
+            dgvMain.Columns.Add("Reg X", "Reg X");
+            dgvMain.Columns.Add("Reg Y", "Reg Y");
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (CommonOpenFileDialog dlg = new CommonOpenFileDialog())
+            {
+                dlg.IsFolderPicker = true;
+
+                if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    // dlg.FileName은 경로 및 확장자 포함
+                    lblPath.Text = dlg.FileName;
+                    SetDataGridView(dlg.FileName);
+
+                    // 폴더만 가져옴(엑셀파일로 저장할 때 이 이름으로 저장)
+                    _currentFileName = Path.GetFileName(dlg.FileName);
+                }
+                else
+                {
+                    _currentFileName = string.Empty;
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void SetDataGridView(string folderPath)
+        {
+            try
+            {
+                // 지정 경로내 모든 파일 가져오기
+                var files = Directory.GetFiles(folderPath).OrderBy(f => f);
+
+                _viaDic.Clear();
+                dgvMain.Rows.Clear();
+
+                foreach (string file in files)
+                {
+                    // 확장자 없이 파일명만 가져와 Split
+                    string strTemp = Path.GetFileNameWithoutExtension(file);
+                    string[] splitString = strTemp.Split('_');
+
+                    // 파일명에 RegX, RegY 있으면 값 가져옴
+                    if (strTemp.IndexOf("RegX", StringComparison.OrdinalIgnoreCase) == -1)
+                    {
+                        continue;
+                    }
+                    else if (strTemp.IndexOf("RegY", StringComparison.OrdinalIgnoreCase) == -1)
+                    {
+                        continue;
+                    }
+
+                    double regX = 0;
+                    double regY = 0; ;
+                    string name = splitString[0];
+
+                    for (int i = 0; i < splitString.Length; i++)
+                    {
+                        // 파일명에 RegX, RegY 있으면 값 가져옴
+                        if (splitString[i].StartsWith("RegX", StringComparison.OrdinalIgnoreCase))
+                        {
+                            regX = double.Parse(splitString[i].Substring(4));
+                        }
+                        else if (splitString[i].StartsWith("RegY", StringComparison.OrdinalIgnoreCase))
+                        {
+                            regY = double.Parse(splitString[i].Substring(4));
+                        }
+                    }
+
+                    // 이름값은 파일명에서 RegX 앞부분까지 할당
+                    name = strTemp.Substring(0, strTemp.IndexOf("RegX", StringComparison.OrdinalIgnoreCase) - 1);
+
+                    // Dictionary 자료 및 화면 할당
+                    _viaDic.Add(name, new Via(name, regX, regY, file));
+                }
+
+                // 인접한 비아 제외 기능 추가 
+                if (ckbGroupingVia.Checked == true)
+                {
+                    try
+                    {
+                        double toleranceX = double.Parse(txtToleranceX.Text);
+                        double toleranceY = double.Parse(txtToleranceY.Text);
+                        _viaDic = PointFilter(_viaDic, toleranceX, toleranceY);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+
+                foreach (var item in _viaDic)
+                {
+                    dgvMain.Rows.Add(item.Value.Name, item.Value.X, item.Value.Y);
+                }
+
+                // Via 이미지 갱신
+                pnlStf.Invalidate();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void dgvMain_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // 데이터 선택하면 이미지 표시
+                if (dgvMain.CurrentRow != null)
+                {
+                    // 선택한 열
+                    DataGridViewRow row = dgvMain.CurrentRow;
+
+                    // 첫번째 열(Name)을 key로
+                    string key = row.Cells[0].Value.ToString();
+
+                    // Dictionary에 저장된 파일 경로로 이미지 로드
+                    picDisplay.Load(_viaDic[key].ImagePath);
+
+                    // Via 이미지 갱신
+                    pnlStf.Invalidate();
+
+                    // 비아 정보 출력
+                    lblName.Text = "Name : " + key;
+                    lblPos.Text = "Pos : " + _viaDic[key].X + ", " + _viaDic[key].Y;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // 원(STF)을 그리고 비아 그리기
+        private void pnlStf_Paint(object sender, PaintEventArgs e)
+        {
+            int centerX = this.pnlStf.Width / 2;
+            int centerY = this.pnlStf.Height / 2;
+            int radius = 150;
+            int diameter = radius * 2;
+
+            e.Graphics.FillEllipse(new SolidBrush(Color.LimeGreen),
+                new System.Drawing.Rectangle(centerX - radius, centerY - radius, diameter, diameter));
+
+            int viaDiameter = 4;
+
+            // 현재 경로에 모든 비아 그리기
+            using (Pen pen = new Pen(Color.Yellow, 1))
+            {
+                foreach (var via in _viaDic)
+                {
+                    e.Graphics.DrawEllipse(pen, centerX + (float)via.Value.X - viaDiameter / 2,
+                        centerY - (float)via.Value.Y - viaDiameter / 2, viaDiameter, viaDiameter);
+                }
+            }
+
+            // 선택된 비아 그리기
+            if (dgvMain.CurrentRow != null)
+            {
+                using (Pen pen = new Pen(Color.Blue, 2))
+                {
+                    // 선택한 열
+                    DataGridViewRow row = dgvMain.CurrentRow;
+
+                    // 첫번째 열(Name)을 key로
+                    string key = row.Cells[0].Value.ToString();
+
+                    e.Graphics.DrawEllipse(pen, centerX + (float)_viaDic[key].X - viaDiameter / 2,
+                        centerY - (float)_viaDic[key].Y - viaDiameter / 2, viaDiameter, viaDiameter);
+                }
+            }
+        }
+
+        private void txtToleranceX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 숫자 또는 Backspace만 입력
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        Dictionary<string, Via> PointFilter(Dictionary<string, Via> source, double toleranceX, double toleranceY)
+        {
+            Dictionary<string, Via> filterDic = new Dictionary<string, Via>();
+
+            foreach (var currentDic in source)
+            {
+                bool isTooClose = false;
+
+                // 인접한 좌표가 필터링된 자료에 있는지 확인
+                foreach (var dic in filterDic)
+                {
+                    if (Math.Abs(currentDic.Value.X - dic.Value.X) <= toleranceX &&
+                        Math.Abs(currentDic.Value.Y - dic.Value.Y) <= toleranceY)
+                    {
+                        isTooClose = true;
+                        break;
+                    }
+                }
+
+                if (isTooClose == false)
+                {
+                    filterDic.Add(currentDic.Key, currentDic.Value);
+                }
+            }
+
+            return filterDic;
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Excel.Application excel = new Excel.Application();
+                if(excel == null)
+                {
+                    MessageBox.Show("엑셀이 설치되지 않았습니다.");
+                    return;
+                }
+
+                if(dgvMain.Rows.Count > 1)
+                {
+                    SaveFileDialog dlg = new SaveFileDialog();
+                    dlg.Title = "Save as Excel File";
+                    dlg.Filter = "Excel Documents (*xls)|*.xls";
+                    dlg.FileName = _currentFileName + ".xls";
+
+                    if(dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+                        Excel.Worksheet workSheet = workbook.Worksheets.get_Item(1) as Excel.Worksheet;
+                        workSheet.Name = "DataView";
+                        excel.DisplayAlerts = false;
+
+                        // 헤더 출력
+                        for (int i = 0; i < dgvMain.Columns.Count - 1; i++)
+                        {
+                            workSheet.Cells[1, i + 1] = dgvMain.Columns[i].HeaderText;
+                        }
+
+                        // 내용 출력
+                        for (int i = 0; i < dgvMain.Rows.Count; i++)
+                        {
+                            for (int j = 0; j < dgvMain.Columns.Count - 1; j++)
+                            {
+                                workSheet.Cells[i + 2, j + 1] = dgvMain.Rows[i].Cells[j].Value;
+                            }
+                        }
+
+                        // 글자 크기에 맞게 셀 크기를 자동으로 조절
+                        workSheet.Columns.AutoFit();
+
+                        // 엑셀 2003 으로만 저장이 됨
+                        workbook.SaveAs(dlg.FileName, Excel.XlFileFormat.xlWorkbookNormal, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                        workbook.Close(Type.Missing, Type.Missing, Type.Missing); excel.Quit();
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
